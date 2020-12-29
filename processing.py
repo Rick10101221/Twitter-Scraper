6c1b0da709b6c774812d848ec7d71461c58c470e
Darien Tsai <smartdragondt@gmail.com>
2020-12-28 17:49:42 -0800

    added averaging function for sentiment values

--- processing.py
+++ processing.py
@@ -1,7 +1,14 @@
-# Performs sentiment analysis on a string
+"""
+Implements all processing functions
+
+Useful things to import:
+- avg_sentiment  | sentiment analysis
+- avg_sentences  | average sentence count 
+- count          | word frequencies
+"""
 from textblob import TextBlob as tb
 
-"""
+"""-----------------------------------------------------------------------------
 String -> Sentiment Scores(Polarity, Subjectivity)
 -1 = negative
 1 = positive
@@ -13,13 +20,30 @@ def sentiment(text):
   return tb(text).sentiment
 
 """
+[String] -> Sentiment Scores(Polarity, Subjectivity)
+Gives an average sentiment value for list of tweets
+"""
+def avg_sentiments(texts):
+  total = len(texts)
+  polarity_total, subjectivity_total = 0, 0
+  
+  # Sums up all the polarity/subjectivity values
+  for i in range(total):
+    sentiments = sentiment(texts[i])
+    polarity_total += sentiments[0]
+    subjectivity_total += sentiments[1]
+  
+  return (polarity_total / total, subjectivity_total / total)
+
+"""-----------------------------------------------------------------------------
 String -> Int
 Counts sentences
 """
 def sentences(text):
   return len(tb(text).sentences)
 
-"""
+
+"""-----------------------------------------------------------------------------
 [String] -> Number
 Gives average sentence count from a list of tweets
 """
@@ -27,20 +51,20 @@ def avg_sentences(texts):
   total = len(texts)
   sentence_count = 0
 
+  # Sums up all sentences in tweets
   for i in range(total):
     sentence_count += sentences(texts[i])
 
   return sentence_count / total
 
-"""
+"""-----------------------------------------------------------------------------
 String, Dictionary{word:frequency} -> Dictionary{word:frequency}
 Case insensitive word frequency given a dictionary of word frequencies
 """
 def count(text, counts):
-  # lower case arg, split to words
   words = text.lower().split()
 
-  # Store word frequencies in dictionary
+  # Update word frequencies to dictionary
   for w in words:
     if not w in counts.keys():
       counts[w] = 1
