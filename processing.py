625365540c1eed9fd8d14ddda842d7ad12993b20
Darien Tsai <smartdragondt@gmail.com>
2020-12-28 17:00:47 -0800

    added QOL function updates

--- processing.py
+++ processing.py
@@ -20,15 +20,27 @@ def sentences(text):
   return len(tb(text).sentences)
 
 """
-String -> Dictionary{word:frequency}
-Case insensitive word frequency
+[String] -> Number
+Gives average sentence count from a list of tweets
 """
-def count(text):
+def avg_sentences(texts):
+  total = len(texts)
+  sentence_count = 0
+
+  for i in range(total):
+    sentence_count += sentences(texts[i])
+
+  return sentence_count / total
+
+"""
+String, Dictionary{word:frequency} -> Dictionary{word:frequency}
+Case insensitive word frequency given a dictionary of word frequencies
+"""
+def count(text, counts):
   # lower case arg, split to words
   words = text.lower().split()
 
   # Store word frequencies in dictionary
-  counts = {}
   for w in words:
     if not w in counts.keys():
       counts[w] = 1
